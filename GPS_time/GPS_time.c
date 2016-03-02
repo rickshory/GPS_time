aa73d70e1d823e016cb1f211779d29638536eae4
Rick Shory <rickshory@gmail.com>
2016-03-01 21:12:34 -0800

    begin simulate GPS power control

--- GPS_time/GPS_time.c
+++ GPS_time/GPS_time.c
@@ -10,9 +10,16 @@
 #include <util/delay.h>
 
 int main(void) {
-  // Set the pin 0 at port B as output
+  // Set the bit 0 of port B as output
   DDRB |= (1<<PB0);
-
+  // Set the bit 1 of port B as output
+  DDRB |= (1<<PB1);
+  // wait 1 second for supply power to stabilize
+  _delay_ms(1000);
+  // simulate turning on power to GPS
+  PORTB |= (1<<PB1);
+  // wait 1 second for GPS power to stabilize
+  _delay_ms(1000);  
   while(1) {    
     // Turn led on by setting corresponding bit high in the PORTB register.
     PORTB |= (1<<PB0);
