ee60b7b8b28477493f84d698d91f046eb9f4f2ee
Rick Shory <rickshory@gmail.com>
2016-03-03 20:22:11 -0800

    changed uC to ATtiny84A

--- GPS_time/GPS_time.c
+++ GPS_time/GPS_time.c
@@ -9,31 +9,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-  // assure port B bit 0 starts out low	
-  PORTB &= ~(1<<PB0);
-  // Set the bit 0 of port B as output
-  DDRB |= (1<<PB0);
-  // assure port B bit 1 starts out low	
-  PORTB &= ~(1<<PB1);  
-  // Set the bit 1 of port B as output
-  DDRB |= (1<<PB1);
-  DDRB |= (1<<PB2); // Port B bit 2 output
-  // wait 1 second for supply power to stabilize
-  _delay_ms(1000);
-  // simulate turning on power to GPS
-  PORTB |= (1<<PB1);
-  // wait 1 second for GPS power to stabilize
-  _delay_ms(1000);
-  // simulate 200ms pulse to wake GPS
-  PORTB |= (1<<PB0); // on
-  _delay_ms(200);
-  PORTB &= ~(1<<PB0); // off
-  while(1) {   
-	  // for now, blink bit2 to show standby 
-    PORTB |= (1<<PB2);
-    _delay_ms(1000);
-    PORTB &= ~(1<<PB2);
-    _delay_ms(1000);
-  }
+// define what pins the LEDs are connected to.
+// PA3 is pin 10 in the PDIP chip package
+#define LED PA3
+
+// Some macros that make the code more readable
+#define output_low(port,pin) port &= ~(1<<pin)
+#define output_high(port,pin) port |= (1<<pin)
+#define set_input(portdir,pin) portdir &= ~(1<<pin)
+#define set_output(portdir,pin) portdir |= (1<<pin)
+
+int main(void)
+{
+	// initialize the direction of PORTD #6 to be an output
+	set_output(DDRA, LED);  
+
+    while(1)
+    {
+		// turn on the LED for 1000ms
+		output_high(PORTA, LED);
+		_delay_ms(1000);
+		// now turn off the LED for another 200ms
+		output_low(PORTA, LED);
+		_delay_ms(1000);
+		// now start over
+    }
 }
